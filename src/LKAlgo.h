951bf2b08ee227c26792b56f4d53a51827bd76ed
caigaoub <caigao@buffalo.edu>
2020-04-21 13:16:17 -0400

    push for saving only

---
+++ src/LKAlgo.h
@@ -0,0 +1,44 @@
+#ifndef _LKALGO_H_
+#define _LKALGO_H_
+
+
+#include "DataHandler.h"
+#include <tuple>
+#include <cmath>
+#include <math.h>
+#include <deque>
+#include <vector>
+#include <numeric>
+#include "ProgTime.h"
+#include <string>
+#define INF numeric_limits<double>::infinity()
+
+
+
+
+
+
+class LK{
+public:
+	int 									_graphsize;
+	vector<vector<double>>					_riskgraph;
+	vector<vector<vector<double>>>			_innerriskgraph;
+
+	vector<int> 
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif 
